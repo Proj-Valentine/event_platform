8459824f1de0100d4a17d98c1e209fbcdc866f11
VKAmpah <vkampah28@gmail.com>
2024-01-04 18:08:39 -0330

    update

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -23,7 +23,7 @@ const EventDetails = async ({params: { id },searchParams}: SearchParamProps) => 
   return (
     <>
       <section className="flex justify-center bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl mt-4">
           <Image
             src={event.imageUrl}
             alt="hero image"
