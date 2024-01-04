e99d31195ff0c815347153844e3efe7cc9ec38bb
VKAmpah <vkampah28@gmail.com>
2024-01-04 05:18:23 -0330

    update

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -22,7 +22,7 @@ const EventDetails = async ({params: { id },searchParams}: SearchParamProps) => 
 
   return (
     <>
-      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <section className="flex justify-center bg-dotted-pattern bg-contain">
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
@@ -32,7 +32,7 @@ const EventDetails = async ({params: { id },searchParams}: SearchParamProps) => 
             className="h-full min-h-[300px] object-cover object-center"
           />
 
-          <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex w-full flex-col gap-8 p-5 md:p-10 dark:text-white">
             <div className="flex flex-col gap-6">
               <h2 className="h2-bold">{event.title}</h2>
 
